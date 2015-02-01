3a1ee54a88b5a789ae79ff156b8080c7191e686c
marc condon <marc@sfdjango.com>
2015-01-31 17:46:47 -0800

    added starter LetterCounter class

---
+++ lib/letter_counter.py
@@ -0,0 +1,26 @@
+__author__ = 'marc'
+"""
+LetterCounter Class - Tracks a list of Letter Classes
+ members
+   letters - list of letters
+   is_counting() - is letter actively being counted
+   append() - add letter class with count of one
+   increment() - increment count on active letter
+"""
+class Letter:
+    letter = ''
+    count = 0
+    def __init__(self, letter):
+        self.letter = letter
+        self.count = 1
+
+class LetterCounter:
+    letters = []
+    def __init__(self, letter):
+        pass
+    def is_counting(self, letter):
+        pass
+    def append(self, letter):
+        pass
+    def increment(self, letter):
+        pass
