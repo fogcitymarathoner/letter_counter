76141a35dbc524b72d8ae541e27ee1a8ffc83b45
marc condon <marc@sfdjango.com>
2015-01-31 22:31:43 -0800

    added test to make sure incrementing a letter not under control does nothing

--- tests/tests_letter_counter_increment.py
+++ tests/tests_letter_counter_increment.py
@@ -19,3 +19,21 @@ class LetterCounterAppendTest(unittest.TestCase):
         self.assertEqual(1, lc.letters[1].count)
         lc.increment('b')
         self.assertEqual(2, lc.letters[1].count)
+
+    def test_increment_letter_not_under_control(self):
+        """
+        test_increment_letter_not_under_control - make sure incrementing letter not under control doesn't affect letters
+        list
+        :return:
+        """
+
+        lc = LetterCounter('a')
+
+        lc.append('b')
+
+        self.assertEqual(2, len(lc.letters))
+        self.assertEqual(1, lc.letters[1].count)
+        lc.increment('b')
+        self.assertEqual(2, lc.letters[1].count)
+        lc.increment('z')
+        self.assertEqual(2, len(lc.letters))
