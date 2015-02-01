6c2fb6c42d555b86501c41878135b3285334926b
marc condon <marc@sfdjango.com>
2015-01-31 17:44:32 -0800

    added class LetterCounter and test for loading modules

---
+++ tests/tests_load_modules.py
@@ -0,0 +1,22 @@
+__author__ = 'marc'
+import unittest
+
+class LetterModuleImportTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_imports(self):
+        """
+        test_imports - makes sure modules will load
+        """
+        module_list = [
+            'lib',
+            'lib.letter_counter',
+            ]
+        for mod in module_list:
+            try:
+                __import__(mod)
+
+            except ImportError:
+                print mod
+                self.assertTrue(False)
